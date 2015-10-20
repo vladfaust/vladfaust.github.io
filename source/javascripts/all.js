fd2607548b9faad0691e31e237086f12a02af130
vladfaust <vladislav.faust@gmail.com>
2015-10-20 20:21:03 +0300

    Initial commit

---
+++ source/javascripts/all.js
@@ -0,0 +1,18 @@
+//= require jquery
+//= require_tree .
+
+function extractDomain(url) {
+    var domain;
+    //find & remove protocol (http, ftp, etc.) and get domain
+    if (url.indexOf("://") > -1) {
+        domain = url.split('/')[2];
+    }
+    else {
+        domain = url.split('/')[0];
+    }
+
+    //find & remove port number
+    domain = domain.split(':')[0];
+
+    return domain;
+}
